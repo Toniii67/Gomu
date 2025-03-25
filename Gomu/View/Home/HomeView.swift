501dfa163afe07bf530e75584c4cd16e1725f732
Toniii67 <francopranara@gmail.com>
2025-03-25 20:15:55 +0700

    first

--- Gomu/View/Home/HomeView.swift
+++ Gomu/View/Home/HomeView.swift
@@ -5,3 +5,14 @@
 //  Created by Franco Antonio Pranata on 25/03/25.
 //
 
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- Gomu/View/Report/ReportView.swift
+++ Gomu/View/Report/ReportView.swift
@@ -5,3 +5,14 @@
 //  Created by Franco Antonio Pranata on 25/03/25.
 //
 
+import SwiftUI
+
+public struct ReportView: View {
+    public var body: some View {
+        Text("ReportView")
+    }
+}
+
+#Preview {
+    ReportView()
+}
